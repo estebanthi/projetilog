8273bfe8b60554a14179913d79ab5f043bf4864f
estebanthi <estebanthi62@gmail.com>
2023-03-01 09:27:35 +0100

    Update index.html

--- src/list.ts
+++ src/list.ts
@@ -4,25 +4,28 @@ interface ListOwner<T> {
     onUpdate(item: T): void;
 }
 
-class GenericList<T> {
+class List<T> extends HTMLElement {
     private items: T[] = [];
     private owner: ListOwner<T>;
 
     constructor(owner: ListOwner<T>) {
+        super();
         this.owner = owner;
     }
 
     add(item: T) {
         this.items.push(item);
         this.owner.onAdd(item);
+        this.render();
     }
 
-    remove(item: T) {
+    remove_(item: T) {
         const index = this.items.indexOf(item);
         if (index !== -1) {
             this.items.splice(index, 1);
             this.owner.onRemove(item);
         }
+        this.render();
     }
 
     update(item: T) {
@@ -31,9 +34,22 @@ class GenericList<T> {
             this.items[index] = item;
             this.owner.onUpdate(item);
         }
+        this.render();
     }
 
     getAllItems(): T[] {
         return this.items;
     }
+
+    render() {
+        console.log('rendering list');
+        this.innerHTML = '';
+        this.items.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = item.toString();
+            this.appendChild(li);
+        });
+    }
 }
+
+customElements.define('my-list', List);
